d75c2c121538393e676030057cbe580cde4c6694
rahulworld <chauhanr371@gmail.com>
2016-10-18 14:06:16 +0530

    final Activity

--- app/src/main/java/org/rahul/voicecalci/MainActivity.java
+++ app/src/main/java/org/rahul/voicecalci/MainActivity.java
@@ -1,15 +1,22 @@
 package org.rahul.voicecalci;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity{
     // IDs of all the numeric buttons
@@ -24,11 +31,14 @@ public class MainActivity extends AppCompatActivity{
     private boolean stateError;
     // If true, do not allow to add another DOT
     private boolean lastDot;
+    private ImageButton btnSpeak;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.btnSpeak= (ImageButton) findViewById(R.id.btnspeak);
         // Find the TextView
         this.txtScreen = (TextView) findViewById(R.id.txtScreen);
         // Find and set OnClickListener to numeric buttons
@@ -116,6 +126,12 @@ public class MainActivity extends AppCompatActivity{
                 onEqual();
             }
         });
+        findViewById(R.id.btnspeak).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                promptSpeechInput();
+            }
+        });
     }
 
     /**
@@ -140,6 +156,44 @@ public class MainActivity extends AppCompatActivity{
                 stateError = true;
                 lastNumeric = false;
             }
+        }
+    }
+    /**
+     * Showing google speech input dialog
+     * */
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+    /**
+     * Receiving speech input
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txtScreen.setText(result.get(0));
+                }
+                break;
+            }
+
         }
     }
 }
